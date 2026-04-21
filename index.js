331632069b2731ffe1aca9052a3d1ccd7887a2fd
Tushar V <tushar.vijayanagar@gmail.com>
2026-04-21 18:18:32 +0530

    Fix: Change port to 8080

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ dotenv.config();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 console.log("PORT FROM ENV:", process.env.PORT);
 
 process.on("exit", (code) => {
